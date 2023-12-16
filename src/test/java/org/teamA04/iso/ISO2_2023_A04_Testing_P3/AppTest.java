9f44397561e1d1ebaef9d4cd09b6cb3f800fbfba
AlejandroHoyo <alejandro.hoyo@alu.uclm.es>
2023-12-16 14:19:45 +0100

    Implementation for thetest case 3: Gravitational attraction force

---
+++ src/test/java/org/teamA04/iso/ISO2_2023_A04_Testing_P3/AppTest.java
@@ -0,0 +1,20 @@
+package org.teamA04.iso.ISO2_2023_A04_Testing_P3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
